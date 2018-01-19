698aa943005916c6f4c77898ea437fbc5df512eb
Linh Nguyen <hoangtuboy95@gmail.com>
2018-01-19 22:00:13 +0700

    init project web

---
+++ MusicBigdataGraduate/src/main/java/vn/edu/tdmu/MusicBigdataGraduateApplication.java
@@ -0,0 +1,12 @@
+package vn.edu.tdmu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicBigdataGraduateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicBigdataGraduateApplication.class, args);
+	}
+}
